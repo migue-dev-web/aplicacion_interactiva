ee782fce4ff1176decb5f697d84625875ba7fe04
migue-dev-web <miguel.castellanos@udgvirtual.udg.mx>
2025-02-03 13:56:49 -0600

    Act 2.1. La primera ventana completa

---
+++ Assets/site1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class site1 : MonoBehaviour
+{
+    public void AbrirUrl1(){
+        Application.OpenURL("https://iconos8.es/icon/set/programaci%C3%B3n/officel");
+    }
+
+    public void AbrirUrl2(){
+        Application.OpenURL("https://blog.febucci.com/2018/10/unity-ui-tutorial/");
+    }
+
+    public void AbrirUrl3(){
+        Application.OpenURL("https://unity.com/es/how-to/organizing-your-project");
+    }
+}
