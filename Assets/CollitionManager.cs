c934eff0397a6e58e11972de00f20829d5273f5d
migue-dev-web <miguel.castellanos@udgvirtual.udg.mx>
2025-02-23 17:01:01 -0600

    Actividad 2.6. El flujo de la interfaz completa

--- Assets/CollitionManager.cs
+++ Assets/CollitionManager.cs
@@ -4,8 +4,28 @@ using System.Collections.Generic;
 
 public class CollitionManager : MonoBehaviour
 {
-        void OnTriggerEnter2D(Collider2D coll)
+
+    private Animator animator;
+    public string isOn = "isOn";
+    
+    void Start()
+    {
+        // Encuentra automáticamente el Animator en el mismo GameObject
+        animator = GetComponent<Animator>();
+
+        // Verifica si el Animator existe
+        if (animator == null)
+        {
+            Debug.LogError("No se encontró el Animator en " + gameObject.name);
+        }
+    }        void OnTriggerEnter2D(Collider2D coll)
         {
             Debug.Log("Chocaron¡¡");
+
+            animator.SetBool(isOn, true);
+        }
+
+        void OnTriggerExit2D(Collider2D coll){
+             animator.SetBool(isOn, false);
         }
 }
